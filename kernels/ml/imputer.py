889c484dff448ea6354c59c089e400743ecf6653
Sylvain Cristofari <sylvaincristofari@MacBook-Pro-de-Sylvain.local>
2018-04-27 13:39:19 +0200

    up

---
+++ kernels/ml/imputer.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class NanImputer(BaseEstimator, TransformerMixin):
+        def fit(self, X, y=None):
+            return self
+        def transform(self, X):
+            return X.fillna('not defined')
